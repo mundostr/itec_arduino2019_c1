c1ce9d5afa92f1a3abb04990b83e9d1be1373068
mundostr <idux.net@gmail.com>
2019-07-23 16:45:01 -0300

    Actualización ejemplo BME280

--- ejercicios/bme280_base/bme280_base.ino
+++ ejercicios/bme280_base/bme280_base.ino
@@ -1,3 +1,9 @@
+/*
+  Ejemplo de uso sensor BME280 (temperatura, presión y humedad ambiente
+  mediante la librería BME280I2C de Tyler Glenn (FiniteSpace)
+  https://github.com/finitespace
+*/
+
 #include <BME280I2C.h>
 #include <Wire.h>
 
@@ -8,22 +14,22 @@ BME280I2C bme;
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  while(!bme.begin()) {
+  while (!bme.begin()) {
     Serial.println("No se detecta el sensor BME280");
     delay(1000);
   }
 }
 
 void loop() {
-   leerSensor();
-   delay(500);
+  leerSensor();
+  delay(500);
 }
 
 void leerSensor () {
-   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
-   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+  BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+  BME280::PresUnit presUnit(BME280::PresUnit_Pa);
 
-   bme.read(pres, temp, hum, tempUnit, presUnit);
+  bme.read(pres, temp, hum, tempUnit, presUnit);
 
-   Serial.println(temp);
+  Serial.println(temp);
 }
